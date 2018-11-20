5353a68134ccc4b6c70817e5260e0b4bed05a3da
MichaelPekala <36052105+MichaelPekala@users.noreply.github.com>
2018-11-19 23:41:20 -0600

    Add files via upload

--- simulator.py
+++ simulator.py
@@ -5,20 +5,23 @@ def twos_convert(line):
             return int(line[16:32], 2)
     
 input_file = open("i_mem.txt", "r")
-print("add, sub, xor, addi, beq, bne, slt, lw, sw")
+file = open("MulticycleInfo.txt","w")
+print("Supported instructions: add, sub, xor, addi, beq, bne, slt, lw, sw\n")
 instList = []
 multi_cycles = 0
-multic_3 = 0
-multic_4 = 0
-multic_5 = 0
+multi_3 = 0
+multi_4 = 0
+multi_5 = 0
 count = 0
+hazard = [-1,-1]
+hazard_c = 0
 for code in input_file:
     line = code.replace("\t", "")
     line = line.replace(" ","")     # remove spaces anywhere in line
     if (line == "\n"):              # empty lines ignored
         continue
     line = line.replace("\n","")
-    line = format(int(line,16), "032b")
+    line = format(int(line,16),"032b")
     instList.append(line)
     
 input_file.close()
@@ -29,25 +32,36 @@ pc = 0
 
 while(pc < len(instList)):
     line = instList[pc]
-        
     #r-type
     if(line[0:6] == '000000'):
         multi_cycles += 4 #rtype have 4 cycles
         #add
         if(line[26:32] == '100000'):
             #rd = rs + rt
+            if ((hazard[0] is (int(line[6:11],2) or int(line[11:16],2))) or 
+            hazard[1] is (int(line[6:11],2) or int(line[11:16],2))):
+              print("HAZARD")
+              hazard_c += 1
             r[int(line[16:21],2)] = r[int(line[6:11],2)] + r[int(line[11:16],2)]
+            file.write("Instruction "+repr(count)+": add - 4 cycles \n")
+            multi_4 += 1
             pc = pc + 1
+            hazard.append(int(line[16:21],2))
+            hazard.pop(0)
            
         #sub
         elif(line[26:32] == '100010'):
             #rd = rs - rt
             r[int(line[16:21],2)] = r[int(line[6:11],2)] - r[int(line[11:16],2)]
+            file.write("Instruction "+repr(count)+": sub - 4 cycles \n")
+            multi_4 += 1
             pc = pc + 1
         #xor
         elif(line[26:32] == '100110'):
             #rd = rs ^ rt
             r[int(line[16:21],2)] = r[int(line[6:11],2)] ^ r[int(line[11:16],2)]
+            file.write("Instruction "+repr(count)+": xor - 4 cycles \n")
+            multi_4 += 1
             pc = pc + 1
         #slt
         elif(line[26:32] == '101010'):
@@ -56,6 +70,8 @@ while(pc < len(instList)):
                 r[int(line[16:21],2)] = 1
             else:
                 r[int(line[16:21],2)] = 0
+            file.write("Instruction "+repr(count)+": slt - 4 cycles \n")
+            multi_4 += 1
             pc = pc + 1
             
     #addi
@@ -64,13 +80,17 @@ while(pc < len(instList)):
         #rt = rs + imm
         imm = twos_convert(line)
         r[int(line[11:16],2)] = r[int(line[6:11],2)] + imm
+        file.write("Instruction "+repr(count)+": addi - 4 cycles \n")
+        multi_4 += 1
         pc = pc + 1
     #beq
     elif(line[0:6] == '000100'):
         multi_cycles += 3
+        multi_3 += 1
         #if(rs == rt) then pc = imm + pc
         temp_pc = pc
         imm = twos_convert(line)
+        file.write("Instruction "+repr(count)+": beq - 3 cycles \n")
         if(r[int(line[6:11],2)] is r[int(line[11:16],2)]):
             pc = pc + imm + 1
         else:
@@ -81,9 +101,11 @@ while(pc < len(instList)):
     #bne
     elif(line[0:6] == '000101'):
         multi_cycles += 3
+        multi_3 += 1
         #if(rs != rt) then pc = imm + pc
         temp_pc = pc
         imm = twos_convert(line)
+        file.write("Instruction "+repr(count)+": bne - 3 cycles \n")
         if(r[int(line[6:11],2)] != r[int(line[11:16],2)]):
             pc = pc + imm + 1
         else:
@@ -93,9 +115,11 @@ while(pc < len(instList)):
     #lw
     elif(line[0:6] == '100011'):
         multi_cycles += 5
+        multi_5 += 1
         #rt = MEM[rs + imm]
         offset = int((r[int(line[6:11],2)] + int(line[16:32],2) - 8192)/4)
         r[int(line[11:16],2)] = memList[offset]
+        file.write("Instruction "+repr(count)+": lw - 5 cycles \n")
         pc = pc + 1
     #sw
     elif(line[0:6] == '101011'):
@@ -103,6 +127,8 @@ while(pc < len(instList)):
         #MEM[rs + imm] = rt
         offset = int((r[int(line[6:11],2)] + int(line[16:32],2) - 8192)/4)
         memList[offset] = r[int(line[11:16],2)]
+        file.write("Instruction "+repr(count)+": sw - 4 cycles \n")
+        multi_4 += 1
         pc = pc + 1  
     else:
         print("Unknown instruction:"+ line)
@@ -110,4 +136,19 @@ while(pc < len(instList)):
     count = count + 1
 
 
-print("Final DIC count is: {}".format(count))
+print("PC:                   " +repr(pc * 4))
+print("Registers 0-7:        " + repr(r))
+print("DIC count:            {}".format(count))
+
+print("\nMulticycle CPU Information ")
+print("Total number of cycles:         " + repr(multi_cycles))
+print("Number of 3 cycle instructions: " + repr(multi_3))
+print("Number of 4 cycle instructions: " + repr(multi_4))
+print("Number of 5 cycle instructions: " + repr(multi_5))
+file.write("\n" + "Total cycles in Multicycle: " + repr(multi_cycles))
+
+print("\nPipelined CPU Information ")
+print("Number of hazards: " + repr(hazard_c))
+print("Total number of cycles: " + repr(count + 4 + hazard_c))
+
+file.close()
